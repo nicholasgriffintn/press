979208caa21fceaee89b0cce219dacfec7148961
Nicholas Griffin <me@nicholasgriffin.co.uk>
2023-06-25 00:44:48 +0100

    chore: updating api setup

--- apps/cms/app/(dashboard)/dashboard/page.tsx
+++ apps/cms/app/(dashboard)/dashboard/page.tsx
@@ -1,13 +1,19 @@
-import { redirect } from "next/navigation"
+import { redirect } from "next/navigation";
 
-import { authOptions } from "@/lib/auth"
-import { db } from "@/lib/db"
-import { getCurrentUser } from "@/lib/session"
-import { EmptyPlaceholder } from "@/components/empty-placeholder"
-import { DashboardHeader } from "@/components/header"
-import { PostCreateButton } from "@/components/post-create-button"
-import { PostItem } from "@/components/post-item"
-import { DashboardShell } from "@/components/shell"
+
+
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { getCurrentUser } from "@/lib/session";
+import { EmptyPlaceholder } from "@/components/empty-placeholder";
+import { DashboardHeader } from "@/components/header";
+import { PostCreateButton } from "@/components/post-create-button";
+import { PostItem } from "@/components/post-item";
+import { DashboardShell } from "@/components/shell";
+
+
+
+
 
 export const metadata = {
   title: "Dashboard",
@@ -20,7 +26,7 @@ export default async function DashboardPage() {
     redirect(authOptions?.pages?.signIn || "/login")
   }
 
-  const posts = await db.post.findMany({
+  const posts = await db.content.findMany({
     where: {
       authorId: user.id,
     },
