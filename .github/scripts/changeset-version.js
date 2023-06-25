89d8ab414d8dc795a7075658d99ad6d596867c45
Nicholas Griffin <me@nicholasgriffin.co.uk>
2023-06-25 14:11:33 +0100

    fix: changeset script import

--- .github/scripts/changeset-version.js
+++ .github/scripts/changeset-version.js
@@ -1,6 +1,7 @@
 // ORIGINALLY FROM CLOUDFLARE WRANGLER:
 // https://github.com/cloudflare/wrangler2/blob/main/.github/changeset-version.js
 
-import { exec } from "child_process";
+const { exec } = require('child_process');
+
 exec("npx changeset version");
 exec("pnpm install");
