bf3da5ae7b7ac4625ea2a3c90d7f83c8a85cd7db
Nicholas Griffin <me@nicholasgriffin.co.uk>
2023-06-25 14:08:50 +0100

    chore(ci): adding changesets (#15)
    * feat: adding changesets
    
    * chore: updating script loc
    
    * chore: format
    
    * chore: updating lock
    
    * chore: updating action/setup

---
+++ .github/scripts/changeset-version.js
@@ -0,0 +1,6 @@
+// ORIGINALLY FROM CLOUDFLARE WRANGLER:
+// https://github.com/cloudflare/wrangler2/blob/main/.github/changeset-version.js
+
+import { exec } from "child_process";
+exec("npx changeset version");
+exec("pnpm install");
