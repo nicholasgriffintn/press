b397e49b1568a4acb9162aca1134510234025fbb
Nicholas Griffin <me@nicholasgriffin.co.uk>
2023-06-25 14:35:46 +0100

    chore: formattting

--- .github/scripts/changeset-version.js
+++ .github/scripts/changeset-version.js
@@ -1,7 +1,7 @@
 // ORIGINALLY FROM CLOUDFLARE WRANGLER:
 // https://github.com/cloudflare/wrangler2/blob/main/.github/changeset-version.js
 
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 
 exec("npx changeset version");
 exec("pnpm install");
