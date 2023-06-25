c618561ccf5104b29fed0595de379bdd4a496923
Nicholas Griffin <me@nicholasgriffin.co.uk>
2023-06-25 03:36:36 +0100

    chore: updating loading panels

--- apps/cms/app/(dashboard)/dashboard/loading.tsx
+++ apps/cms/app/(dashboard)/dashboard/loading.tsx
@@ -1,14 +1,11 @@
 import { DashboardHeader } from "@/components/header"
-import { PostCreateButton } from "@/components/post-create-button"
 import { PostItem } from "@/components/post-item"
 import { DashboardShell } from "@/components/shell"
 
 export default function DashboardLoading() {
   return (
     <DashboardShell>
-      <DashboardHeader heading="Posts" text="Create and manage posts.">
-        <PostCreateButton />
-      </DashboardHeader>
+      <DashboardHeader heading="Dashboard" text="Select a site to manage." />
       <div className="divide-border-200 divide-y rounded-md border">
         <PostItem.Skeleton />
         <PostItem.Skeleton />
--- apps/cms/app/(sites)/sites/[siteId]/loading.tsx
+++ apps/cms/app/(sites)/sites/[siteId]/loading.tsx
@@ -1,18 +1,21 @@
-import { Skeleton } from "@/components/ui/skeleton"
+import { Skeleton } from "@/components/ui/skeleton";
+import { PostItem } from "@/components/post-item"
+import { DashboardShell } from "@/components/shell"
 
-export default function Loading() {
+export default function DashboardLoading() {
   return (
-    <div className="grid w-full gap-10">
+    <DashboardShell>
       <div className="flex w-full items-center justify-between">
         <Skeleton className="h-[38px] w-[90px]" />
         <Skeleton className="h-[38px] w-[80px]" />
       </div>
-      <div className="mx-auto w-[800px] space-y-6">
-        <Skeleton className="h-[50px] w-full" />
-        <Skeleton className="h-[20px] w-2/3" />
-        <Skeleton className="h-[20px] w-full" />
-        <Skeleton className="h-[20px] w-full" />
+      <div className="divide-border-200 divide-y rounded-md border">
+        <PostItem.Skeleton />
+        <PostItem.Skeleton />
+        <PostItem.Skeleton />
+        <PostItem.Skeleton />
+        <PostItem.Skeleton />
       </div>
-    </div>
+    </DashboardShell>
   )
 }
