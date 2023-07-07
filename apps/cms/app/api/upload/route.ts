b51ea908ca914ee4f0792ecd03d4154a9b361d39
Nicholas Griffin <me@nicholasgriffin.co.uk>
2023-07-07 02:39:37 +0100

    chore: don't require vercel blob

--- apps/cms/app/api/upload/route.ts
+++ apps/cms/app/api/upload/route.ts
@@ -7,22 +7,18 @@ import { env } from "@/env.mjs";
 export const runtime = "edge";
 
 export async function POST(req: Request) {
-  if (!env.BLOB_READ_WRITE_TOKEN) {
-    return new Response(
-      "Missing BLOB_READ_WRITE_TOKEN. Don't forget to add that to your .env file.",
-      {
-        status: 401,
-      }
-    );
-  }
-
   const file = req.body || "";
   const contentType = req.headers.get("content-type") || "text/plain";
   const filename = `${nanoid()}.${contentType.split("/")[1]}`;
-  const blob = await put(filename, file, {
-    contentType,
-    access: "public",
-  });
 
-  return NextResponse.json(blob);
+  if (env.BLOB_READ_WRITE_TOKEN) {
+    const blob = await put(filename, file, {
+      contentType,
+      access: "public",
+    });
+
+    return NextResponse.json(blob);
+  }
+
+  return NextResponse.json({ ok: false });
 }
